97b93f5a59a9f5389a49a77b8c4f58fb7c79c62d
John McLean <mclean.jb@gmail.com>
2022-04-08 13:17:53 -0400

    Menu toggle fix.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,20 +2,24 @@ import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-import './Navbar.scss'
+import './Navbar.scss'    
+
+const variants = {
+        open: { opacity: 1, x: 0},
+        closed: { opacity: 0, x: "+100%"},
+    }
+
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
+
+
 
     return (
         <nav className="app__navbar">
             <div className="app__navbar-logo">
                 <h5>Weddings Across NC</h5>
             </div>
-            {/* <div className="app__navbar-links">
-            {['home', 'about', 'testimonials', 'services', 'associates', 'contact'].map((item) => (
-                    <a  className="app__flex p-text" key={`link-${item}`} href={`#${item}`}>{item}</a>
-                ))}
-            </div> */}
+
             <ul className="app__navbar-links">
                 {['home', 'about', 'testimonials', 'services', 'associates', 'contact'].map((item) => (
                     <li className="app__flex p-text" key={`link-${item}`}>
@@ -26,12 +30,13 @@ const Navbar = () => {
             </ul>
             <div className='app__navbar-menu'>
                 <HiMenuAlt4 onClick={() => setToggle(true)} />
-                {console.log(toggle)}
                 {
-                    toggle && (
+                   (
                         <motion.div
-                            whileInView={{ x: [300, 0] }}
-                            transition={{ duration: 0.55, ease: 'easeOut' }}
+                            animate={toggle ? "open" : "closed"}
+                            variants={variants}
+                            // whileInView={{ x: [300, 0] }}
+                            // transition={{ duration: 0.85, ease: 'easeOut' }}
                         >
                             <HiX onClick={() => setToggle(false)} />
                             <ul className="app__navbar-links">
