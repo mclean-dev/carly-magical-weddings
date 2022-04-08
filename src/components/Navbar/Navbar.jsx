afa178675c83df251932ebd01f1008552fe24084
John McLean <mclean.jb@gmail.com>
2022-04-08 12:51:35 -0400

    Change menu duration.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
 import './Navbar.scss'
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
@@ -27,12 +26,12 @@ const Navbar = () => {
             </ul>
             <div className='app__navbar-menu'>
                 <HiMenuAlt4 onClick={() => setToggle(true)} />
-
+                {console.log(toggle)}
                 {
                     toggle && (
                         <motion.div
                             whileInView={{ x: [300, 0] }}
-                            transition={{ duration: 0.85, ease: 'easeOut' }}
+                            transition={{ duration: 0.55, ease: 'easeOut' }}
                         >
                             <HiX onClick={() => setToggle(false)} />
                             <ul className="app__navbar-links">
