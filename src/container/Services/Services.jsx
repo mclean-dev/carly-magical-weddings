946521bd387cda296b0f433b2ebe8f04ef00f3e1
John McLean <mclean.jb@gmail.com>
2022-04-11 14:16:18 -0400

    Minor fixes.

--- src/container/Services/Services.jsx
+++ src/container/Services/Services.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 
 import { AppWrap, MotionWrap } from '../../wrapper'
-import { urlFor, client } from '../../client'
+import { client } from '../../client'
 
 import './Services.scss'
 
