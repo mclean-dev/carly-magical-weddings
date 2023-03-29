318ec982c99690d870855a0ef93aca0ce8be11b0
John McLean <john@sounddevelopment.co>
2023-03-29 19:52:59 -0400

    Change form placeholder

--- frontend-react/src/container/Footer/Footer.jsx
+++ frontend-react/src/container/Footer/Footer.jsx
@@ -84,7 +84,7 @@ const Footer = () => {
         <form className="app__footer-form app__flex" name="contact" method="POST" onSubmit={handleSubmit}>
           <input type="hidden" name="form-name" value="contact" />
           <div className="app__flex">
-            <input type="text" className="p-text" placeholder='Your Name and Pronouns' name="name" value={name} onChange={handleChangeInput} required />
+            <input type="text" className="p-text" placeholder='Your Name(s) and Pronouns' name="name" value={name} onChange={handleChangeInput} required />
           </div>
           <div className="app__flex">
             <input type="email" className="p-text" placeholder='Your Email' name="email" value={email} onChange={handleChangeInput} required />
